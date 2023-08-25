36558302d0d27d2c40730f26f4e9fac5ecd3a135
Lars Nicht <larsnicht@web.de>
2023-08-25 10:40:32 +0200

    added support for IndividualAgent (endpoints, db, conroller, service) WIP doubles in DB (e.g. Patch PGL and PGL Gold 3x)

--- src/main/java/com/csInventoryIntellect/SkinVaultAnalytics/repository/PatchRepository.java
+++ src/main/java/com/csInventoryIntellect/SkinVaultAnalytics/repository/PatchRepository.java
@@ -3,5 +3,10 @@ package com.csInventoryIntellect.SkinVaultAnalytics.repository;
 import com.csInventoryIntellect.SkinVaultAnalytics.model.Patch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PatchRepository extends JpaRepository<Patch, Long> {
+
+    Optional<Patch> searchByPatchName(String patchName);
+
 }
